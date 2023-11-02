2213df0243790fce782f275e46fb71fa7f70d8b9
Dillan <dillanrosa@gmail.com>
2023-11-02 18:31:05 -0300

    Add files via upload

--- src/Principal.java
+++ src/Principal.java
@@ -1,16 +1,10 @@
 import br.com.reclamabus.modelos.*;
 import java.util.Scanner;
 
-
 public class Principal {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         PublicoGeral pg = new PublicoGeral();
-        pg.setNome(System.out.println(sc.nextLine));
-
-        pg.exibeConta();
+        pg.exibeMenu();
     }
-
-
-
 }
