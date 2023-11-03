9941cc88d22bbcab16e85a2c389036a779ae637e
Diogo Garcia <124640510+devgarciadiogo@users.noreply.github.com>
2023-11-02 21:14:01 -0300

    Add files via upload

---
+++ reclamabus-main/reclamabus-main/src/br/com/reclamabus/modelos/Idosos.java
@@ -0,0 +1,13 @@
+package br.com.reclamabus.modelos;
+
+public class Idosos extends PublicoGeral {
+    private boolean idoso;
+
+    public boolean getIdoso(){
+        return idoso;
+    }
+
+    public void setIdoso(){
+        this.idoso = idoso;
+    }
+}
---
+++ reclamabus-main/reclamabus-main/src/br/com/reclamabus/modelos/PCD.java
@@ -0,0 +1,13 @@
+package br.com.reclamabus.modelos;
+
+public class PCD extends PublicoGeral {
+    private boolean pcd;
+
+    public boolean getPCD(){
+        return pcd;
+    }
+
+    public void setPCD(){
+        this.pcd = pcd;
+    }
+}
